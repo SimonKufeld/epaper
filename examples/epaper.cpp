9e18ef2e3624c0b0a8652fd1468cb4e137f44842
Simon Kufeld <simonkufeld@gmail.com>
2022-03-28 13:57:22 +0000

    check-in all local files

---
+++ examples/epaper.cpp
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+// Should match with your epaper module, size
+//#include <gdep015OC1.h>  // 1.54 old version
+#include <gdeh0154d67.h>
+//#include "wave12i48.h"
+//#include <gdew042t2.h>  // Tested correctly 06.06.20
+//#include <gdew0583t7.h>
+//#include <gdew0213i5f.h>
+//#include <gdew027w3.h>
+//#include <gdeh0213b73.h>
+
+// Color
+//#include <gdew0583z21.h>
+//#include <gdeh042Z96.h>
+//#include <gdeh042Z21.h>
+// Multi-SPI 4 channels EPD only
+// Please note that in order to use this big buffer (160 Kb) on this display external memory should be used
+// Otherwise you will run out of DRAM very shortly!
+/* Epd4Spi io;
+Wave12I48 display(io); */
+
+// Single SPI EPD
+EpdSpi io;
+//Gdew075T7 display(io);
+//Gdep015OC1 display(io);
+Gdew0213i5f display(io);
+
+// FONT used for title / message body - Only after display library
+//Converting fonts with ümlauts: ./fontconvert *.ttf 18 32 252
+#include <Fonts/ubuntu/Ubuntu_M12pt8b.h>
+
+extern "C"
+{
+   void app_main();
+}
+void delay(uint32_t millis) { vTaskDelay(millis / portTICK_PERIOD_MS); }
+
+
+void demo(uint16_t bkcolor, uint16_t fgcolor)
+{
+   display.fillScreen(bkcolor);
+   display.setTextColor(fgcolor);
+   display.setCursor(10, 40);
+   display.setFont(&Ubuntu_M12pt8b);
+   display.println("CalEPD display test\n");
+   // Print all character from an Adafruit Font
+   if (true)
+   {
+      for (int i = 40; i <= 126; i++)
+      {
+         display.write(i); // Needs to be >32 (first character definition)
+      }
+   }
+}
+
+void app_main(void)
+{
+   printf("CalEPD version: %s\n", CALEPD_VERSION);
+   // Test Epd class
+   display.init(true);
+   display.update();
+   //display.setRotation(2);
+   //display.update();return;
+   //delay(1000);
+
+      // Sizes are calculated dividing the screen in 4 equal parts it may not be perfect for all models
+   uint8_t rectW = display.width()/4; // For 11 is 37.
+
+   uint16_t foregroundColor = EPD_WHITE;
+   // Make some rectangles showing the different colors or grays
+   if (display.colors_supported>1) {
+      printf("display.colors_supported:%d\n", display.colors_supported);
+      foregroundColor = EPD_RED;
+   }
+   
+   //return;
+   display.fillScreen(EPD_WHITE);
+   uint16_t firstBlock = display.width()/4;
+   display.fillRect(    1,1,rectW, firstBlock,foregroundColor);
+   display.fillRect(rectW,1,rectW, firstBlock,EPD_WHITE);
+   display.fillRect(rectW*2,1,rectW,firstBlock,foregroundColor); 
+   display.fillRect(rectW*3,1,rectW-2,firstBlock,EPD_WHITE);
+
+   display.fillRect(    1,firstBlock,rectW,firstBlock,EPD_BLACK);
+   display.fillRect(rectW,firstBlock,rectW,firstBlock,foregroundColor);
+   display.fillRect(rectW*2,firstBlock,rectW,firstBlock,EPD_BLACK); 
+   display.fillRect(rectW*3,firstBlock,rectW-2,firstBlock,foregroundColor);
+
+   display.setCursor(display.width()/2-130,display.height()-90);
+   display.setTextColor(EPD_WHITE);
+   display.setFont(&Ubuntu_M12pt8b);
+   display.println("BERLIN");
+   display.setTextColor(EPD_BLACK);
+   display.println("demo-epaper.cpp full update is done!");
+   display.update();
+   // Leave the epaper White ready for storage
+   delay(2000);
+   display.fillScreen(EPD_WHITE);
+   display.update();
+
+   printf("display: We are done here");
+}
